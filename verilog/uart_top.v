eb562610a4048767e7a28fd2b05c696326de8af7
Will Topping <red@localhost.localdomain>
2016-04-10 22:15:03 -0500

    Trying new uart core, with built in commands stream.

--- verilog/uart_top.v
+++ verilog/uart_top.v
@@ -1,4 +1,5 @@
 `timescale 1ns / 1ps
+`define LOOPBACK
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -18,64 +19,58 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module uart_top(
-		output [5:0] LEDS,
-		input CLK_0, CLK_1,
-		input IDC13,IDC12,IDC11,IDC10,IDC9,IDC8,IDC7,IDC6,IDC5,IDC4,IDC2,IDC1,IDC0,
-		output IDC3,
-		output [16:0] SMA_N,SMA_P
-    );
-	 
+
 	 //IDC starts at pin numbering 7-20 on the header 
 	 // -pins 21 and 22 are ground
-	 // -pins 1-6 are 5V and I2C signals (unused, and should not be connected)
+	 // -pins 1-6 (pi) are 5V and I2C signals (unused, and should not be connected)
 	 // -pins 1-6 on the Hanyang board are also connected to the LEDS
 	 // --- This creates a dual driver scenario and those IDC pins should be left unconnected.
-	 
+	 // --- or routed as to be read by the pi
+
+ module uart_top(
+	 output [5:0] LEDS,
+	 input CLK_0, CLK_1,
+	 input IDC13,IDC12,IDC11,IDC10,IDC9,IDC8,IDC7,IDC6,IDC5,IDC4,IDC2,IDC1,IDC0,
+	 output IDC3,
+	 output [16:0] SMA_N,SMA_P
+	 );
+
 	 // Note: The tx and rx need to be reversed compared to the raspberry pi
-	wire tx_pin;
-	wire rx_pin = IDC1; //raspberry pin #8  (tx) - IDC pin #8
-	
-	assign IDC3 = tx_pin; //raspberry pin #10 (rx) - IDC pin #10
-	assign tx_pin = rx_pin; //Loopback
+	 wire tx_pin;
+	 wire rx_pin = IDC1; //raspberry pin #8  (tx) - IDC pin #8
+	 assign IDC3 = tx_pin; //raspberry pin #10 (rx) - IDC pin #10
 
+	 wire test_pin0 = IDC4; //raspberry pin #11 (GPIO #0)
+	 wire test_pin1 = IDC5; //raspberry pin #12 (GPIO #1)
 
-	wire test_pin0 = IDC4; //raspberry pin #11 (GPIO #0)
-	wire test_pin1 = IDC5; //raspberry pin #12 (GPIO #1)
-	
-	wire [7:0] tx_in,rx_out;
-	wire rx_done;
-	wire tx_send;
+	 `ifdef LOOPBACK
+		 assign tx_pin = rx_pin; //Loopback
+	 `else
+		 uartController uartControls(
+			 .clk(CLK_0),
+			 .rst(test_pin0),
+			 .rx_pin(rx_pin),
+			 .tx_pin(tx_pin)
+		 );
+	 `endif
 
+	 assign LEDS[0] = test_pin0;
+	 assign LEDS[1] = test_pin1;
+	 assign LEDS[2] = 0;
+	 assign LEDS[3] = 1;
+	 assign LEDS[4] = tx_pin;
+	 assign LEDS[5] = rx_pin;
 
-	 uartController uartControls(
-    .clk(CLK_0),
-	 .rst(test_pin0),
-	 .rx_pin(),
-	 .tx_pin(),
-	 .tx_start(tx_send),
-	 .rx_recieved(rx_done),
-    .tx_input(tx_in),
-    .rx_output(rx_out)
-    );
+	 /*
+	 reg [31:0] led_counter;
 
-assign LEDS[0] = test_pin0;
-assign LEDS[1] = test_pin1;
-assign LEDS[2] = 0;
-assign LEDS[3] = 1;
-assign LEDS[4] = tx_pin;
-assign LEDS[5] = rx_pin;
+	 always @(posedge CLK_0)
+	 begin
+		 led_counter <= led_counter + 1;
+	 end
 
-/*
-reg [31:0] led_counter;
-
-always @(posedge CLK_0)
-begin
-	led_counter <= led_counter + 1;
-end
-
-assign LEDS[5] = led_counter[10];
-*/
+	 assign LEDS[5] = led_counter[10];
+	 */
 
 
 endmodule
