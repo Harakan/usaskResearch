0477692dcc46a9a5efa2bd8e3dfb5e5bf9b69c74
Will Topping <fake@email.com>
2016-04-28 18:30:17 +0000

    Initial commit with binary framing for serial communication

--- rasberryPi/hanyang_serial.py
+++ rasberryPi/hanyang_serial.py
@@ -2,12 +2,57 @@
 import serial
 
 baud = 115200
-ser = serial.Serial('/dev/ttyS0',baud)
+ser = serial.Serial('/dev/ttyS0',baudrate=baud,stopbits=serial.STOPBITS_TWO)
 ser.close()
 ser.open()
 
-while(1):
-	print("Writng a ting to serial")
-	ser.write(raw_input()+"\n")
-	print("What bounced back:")
-	print(ser.readline())
+def raw_write():
+	print("Writing this to serial:")
+	ser.write(raw_input().strip()+'\n')
+	print("\nWhat bounced back:")
+	chars_buffered = ser.inWaiting()
+	print("Have {0} chars waiting".format(chars_buffered))
+	chars_read = ser.read(chars_buffered)
+	for char in (chars_read):
+		print ord(char)
+
+def writeCmd(address,data,length=1,ack=False):
+''' Write to register(s) defined at address+length (inclusive)
+		Length of 1 is single register, max value is 255 (value of 0 means length=256)'''
+
+	if length == 0:
+		assert len(data)==256
+	else:
+		assert len(data)==length
+
+	if length != 1:
+		auto_increment = True
+	else:
+		auto_increment = False
+	write_cmd = 0b00100000
+	write_cmd |= auto_increment<1
+	write_cmd |= ack
+	frame=[0,write_cmd,addr_high,addr_low,length]
+
+def readCmd(address,length=1,ack=False):
+''' Read register(s) defined at address+length (inclusive)
+		Length of 1 is single register, max value is 255 (value of 0 means length=256)'''
+	if length != 1:
+		auto_increment = True
+	else:
+		auto_increment = False
+	read_cmd = 0b00010000
+	write_cmd |= auto_increment<1
+	write_cmd |= ack
+	addr_high=(address>>8)&0xFF
+	addr_low=(address&0xFF)
+	frame=[0,read_cmd,addr_high,addr_low,length]
+
+def sendFrame(frame,data=[]):
+	'''Sends Binary data frame as parsed by readCmd|writeCmd'''
+	pass
+
+
+if __name__ == "__main__":
+	print "Starting script"
+	
